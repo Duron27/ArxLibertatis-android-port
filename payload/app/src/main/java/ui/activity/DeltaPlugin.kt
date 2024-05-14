f4fe90b8b559ec9944c43565fe9b6e12e93108ff
Jared Davenport <duron27@gmail.com>
2024-05-14 16:35:54 +0000

    groundcoverify

--- payload/app/src/main/java/ui/activity/DeltaPlugin.kt
+++ payload/app/src/main/java/ui/activity/DeltaPlugin.kt
@@ -1,5 +1,6 @@
 package ui.activity
 
+import android.annotation.SuppressLint
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
@@ -26,105 +27,158 @@ import java.io.StringWriter
 class DeltaPluginActivity : AppCompatActivity() {
 
     private lateinit var shellOutputTextView: TextView
-        private lateinit var deltaPluginButton: Button
-            private lateinit var copyButton: Button // New Button for copying
-                private val handler = Handler(Looper.getMainLooper())
-                private var prefs: SharedPreferences? = null
-                    private val updateTextRunnable = Runnable { updateTextView() }
+    private lateinit var deltaPluginButton: Button
+    private lateinit var deltaGrassButton: Button
+    private lateinit var deltaQueryButton: Button
+    private lateinit var copyButton: Button // New Button for copying
+    private val handler = Handler(Looper.getMainLooper())
+    private var prefs: SharedPreferences? = null
+    private val updateTextRunnable = Runnable { updateTextView() }
 
-                    override fun onCreate(savedInstanceState: Bundle?) {
-                        super.onCreate(savedInstanceState)
-                        setContentView(R.layout.delta_plugin_view)
-                        val commandInput = findViewById<EditText>(R.id.command_input)
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.delta_plugin_view)
+        val commandInput = findViewById<EditText>(R.id.command_input)
 
-                        shellOutputTextView = findViewById(R.id.myTextView)
-                        deltaPluginButton = findViewById(R.id.delta_plugin_button)
-                        copyButton = findViewById(R.id.copy_button) // Reference the new button
+        shellOutputTextView = findViewById(R.id.myTextView)
+        deltaPluginButton = findViewById(R.id.delta_plugin_button)
+        deltaGrassButton = findViewById(R.id.delta_grass_button)
+        deltaQueryButton = findViewById(R.id.delta_query_button)
+        copyButton = findViewById(R.id.copy_button) // Reference the new button
 
-                        commandInput.setOnEditorActionListener { _, actionId, _ ->
-                            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                                executeCommand()
-                                return@setOnEditorActionListener true
-                            }
-                            false
-                        }
+        commandInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                executeCommand()
+                return@setOnEditorActionListener true
+            }
+            false
+        }
 
-                        deltaPluginButton.setOnClickListener {
-                            executeCommand()
-                        }
+        deltaPluginButton.setOnClickListener {
+            executeCommand()
+        }
 
-                        copyButton.setOnClickListener {
-                            copyTextToClipboard()
-                        }
+        deltaGrassButton.setOnClickListener {
+            executeSpecialCommand()
+        }
+
+        deltaQueryButton.setOnClickListener {
+            executeQueryCommand()
+        }
+
+        copyButton.setOnClickListener {
+            copyTextToClipboard()
+        }
+    }
+
+    private fun executeCommand() {
+        val gamePath = PreferenceManager.getDefaultSharedPreferences(ctx)
+                .getString("game_files", "")!!
+        prefs = PreferenceManager.getDefaultSharedPreferences(this)
+
+        var lines = File(Constants.USER_CONFIG + "/delta.cfg").readLines().toMutableList()
+        lines.removeAll { it.contains("content=delta-merged.omwaddon") }
+        lines.removeAll { it.contains("content=builtin.omwscripts") }
+        lines.removeAll { it.contains("data=\"$gamePath/Data Files\"") }
+
+        // Write the modified lines back to the file
+        File(Constants.USER_CONFIG + "/delta.cfg").writeText(lines.joinToString("\n"))
+
+        val newFilePath = Constants.USER_CONFIG + "/delta.cfg" // Create a new path for delta.cfg
+        val deltaoutput = "data=\"$gamePath/Data Files\""
+        File(newFilePath).appendText("\n" + deltaoutput) // Append data to the copied delta.cfg
+
+        val command = "-c " + Constants.USER_CONFIG + "/delta.cfg merge --skip-cells " + findViewById<EditText>(R.id.command_input).text.toString() + " " + gamePath + "/'Data Files'/delta-merged.omwaddon"
+
+        var lines2 = File(Constants.USER_CONFIG + "/openmw.cfg").readLines().toMutableList()
+        lines2.removeAll { it.contains("content=\"$gamePath/Data Files/delta-merged.omwaddon\"") }
+
+        // Write the modified lines back to the file
+        File(Constants.USER_CONFIG + "/openmw.cfg").writeText(lines2.joinToString("\n"))
+
+        val deltamergeoutput = "content=\"$gamePath/Data Files/delta-merged.omwaddon\""
+        File(Constants.USER_CONFIG + "/openmw.cfg").appendText("\n" + deltamergeoutput) // Append data to the copied delta.cfg
+
+        val workingDir = "/data/data/$packageName/files/resources/"
+        val output = shellExec(command, workingDir)
+        shellOutputTextView.text = output // Set the entire text view content
+    }
+
+    // New function for the second button
+    private fun executeSpecialCommand() {
+        val gamePath = PreferenceManager.getDefaultSharedPreferences(ctx)
+                .getString("game_files", "")!!
+        prefs = PreferenceManager.getDefaultSharedPreferences(this)
+
+        var lines = File(Constants.USER_CONFIG + "/openmw.cfg").readLines().toMutableList()
+        lines.removeAll { it.contains("content=\"$gamePath/Data Files/output_groundcover.omwaddon\"") }
+        lines.removeAll { it.contains("content=\"$gamePath/Data Files/output_deleted.omwaddon\"") }
+
+        // Write the modified lines back to the file
+        File(Constants.USER_CONFIG + "/openmw.cfg").writeText(lines.joinToString("\n"))
+
+        // Define the specific commands to execute for this button here
+        val Command = "-c " + Constants.USER_CONFIG + "/delta.cfg filter --all --output $gamePath/'Data Files'/output_groundcover.omwaddon --desc \"Generated groundcover plugin from your local cavebros\" match Static --id \"grass|kelp|lilypad\" --modify model \"^\" \"grass\\\\\" match Cell --cellref-object-id \"grass|kelp|lilypad\"" + " && " + "./delta_plugin -c " + Constants.USER_CONFIG + "/delta.cfg filter --all --output $gamePath/'Data Files'/output_deleted.omwaddon match Static --id \"grass|kelp|lilypad\" --modify model \"^\" \"grass\\\\\" match Cell --cellref-object-id \"grass|kelp|lilypad\" --delete"
+        val specialWorkingDir = "/data/data/$packageName/files/resources/"
+
+        val deltagroundoutput = "content=\"$gamePath/Data Files/output_groundcover.omwaddon\""
+        File(Constants.USER_CONFIG + "/openmw.cfg").appendText("\n" + deltagroundoutput)
+        val deltagrounddeleteoutput = "content=\"$gamePath/Data Files/output_deleted.omwaddon\""
+        File(Constants.USER_CONFIG + "/openmw.cfg").appendText("\n" + deltagrounddeleteoutput)
+
+        val output = shellExec(Command, specialWorkingDir)
+        shellOutputTextView.text = output // Set the entire text view content (optional, can append if preferred)
+    }
+
+    // New function for the second button
+    private fun executeQueryCommand() {
+        // Define the specific commands to execute for this button here
+        val Command = "query " + findViewById<EditText>(R.id.command_input).text.toString()
+        val specialWorkingDir = "/data/data/$packageName/files/resources/" // Optional, use same if not needed
+        val output = shellExec(Command, specialWorkingDir)
+        shellOutputTextView.text = output // Set the entire text view content (optional, can append if preferred)
+    }
+
+    private fun updateTextView() {
+        val output = shellExec()
+        // Append the new output to the existing text
+        shellOutputTextView.append(output)
+        // Schedule another update after a short delay (e.g., 1 second)
+        handler.postDelayed(updateTextRunnable, 1000)
+    }
+
+    private fun shellExec(cmd: String? = null, workingDir: String? = null): String {
+        var output = ""
+        var inputStreamReader: BufferedReader? = null
+        var errorStreamReader: BufferedReader? = null
+
+            try {
+                val processBuilder = ProcessBuilder()
+                    if (workingDir != null) {
+                        processBuilder.directory(File(workingDir))
+                    }
+                System.setProperty("HOME", "/data/data/$packageName/files/")
+                // Build the command with relative path (use the copied delta.cfg)
+                val commandToExecute = arrayOf("/system/bin/sh", "-c", "export HOME=/data/data/$packageName/files/; chmod u+x ./delta_plugin; ./delta_plugin $cmd")
+                processBuilder.command(*commandToExecute)
+                processBuilder.redirectErrorStream(true) // Merge stderr into stdout
+                val process = processBuilder.start()
+
+                inputStreamReader = BufferedReader(InputStreamReader(process.inputStream))
+                errorStreamReader = BufferedReader(InputStreamReader(process.errorStream))
+
+                var line: String?
+                    while (inputStreamReader.readLine().also { line = it } != null) {
+                        output += line + "\n" // Add newline for better formatting
                     }
 
-                    private fun executeCommand() {
-                        val command = findViewById<EditText>(R.id.command_input).text.toString()
-                        val workingDir = "/data/data/$packageName/files/resources/"
-                        val output = shellExec(command, workingDir)
-                        shellOutputTextView.text = output // Set the entire text view content
-                    }
+                    // Check for errors in the error stream
+                        while (errorStreamReader.readLine().also { line = it } != null) {
+                            output += "Rust Error: $line\n" // Prefix Rust errors for clarity
+                        }
 
-                    private fun updateTextView() {
-                        val output = shellExec()
-                        // Append the new output to the existing text
-                        shellOutputTextView.append(output)
-                        // Schedule another update after a short delay (e.g., 1 second)
-                        handler.postDelayed(updateTextRunnable, 1000)
-                    }
-
-                    private fun shellExec(cmd: String? = null, workingDir: String? = null): String {
-                        var output = ""
-                        var inputStreamReader: BufferedReader? = null
-                        var errorStreamReader: BufferedReader? = null
-                        val gamePath = PreferenceManager.getDefaultSharedPreferences(ctx)
-                        .getString("game_files", "")!!
-                        prefs = PreferenceManager.getDefaultSharedPreferences(this)
-
-                        var lines = File(Constants.USER_CONFIG + "/delta.cfg").readLines().toMutableList()
-                        lines.removeAll { it.contains("content=delta-merged.omwaddon") }
-                        lines.removeAll { it.contains("content=builtin.omwscripts") }
-                        lines.removeAll { it.contains("fallback=") }
-
-                        // Write the modified lines back to the file
-                        File(Constants.USER_CONFIG + "/delta.cfg").writeText(lines.joinToString("\n"))
-
-                        val newFilePath = Constants.USER_CONFIG + "/delta.cfg" // Create a new path for delta.cfg
-                        val addonFilePath = gamePath + "/'Data Files'" + "/delta-merged.omwaddon"
-
-                        val deltaoutput = "data=\"$gamePath/Data Files\""
-                        File(newFilePath).appendText("\n" + deltaoutput) // Append data to the copied delta.cfg
-
-                        try {
-                            val processBuilder = ProcessBuilder()
-                            if (workingDir != null) {
-                                processBuilder.directory(File(workingDir))
-                            }
-                            System.setProperty("HOME", "/data/data/$packageName/files/")
-                            // Build the command with relative path (use the copied delta.cfg)
-                            val commandToExecute = arrayOf("/system/bin/sh", "-c", "export HOME=/data/data/$packageName/files/; chmod u+x ./delta_plugin; ./delta_plugin -c $newFilePath merge --skip-cells $cmd $addonFilePath")
-                            processBuilder.command(*commandToExecute)
-                            processBuilder.redirectErrorStream(true) // Merge stderr into stdout
-                            val process = processBuilder.start()
-
-                            val deltamergeoutput = "content=\"$gamePath/Data Files/delta-merged.omwaddon\""
-                            File(Constants.USER_CONFIG + "/openmw.cfg").appendText("\n" + deltamergeoutput) // Append data to the copied delta.cfg
-
-
-                            inputStreamReader = BufferedReader(InputStreamReader(process.inputStream))
-                            errorStreamReader = BufferedReader(InputStreamReader(process.errorStream))
-
-                            var line: String?
-                            while (inputStreamReader.readLine().also { line = it } != null) {
-                                output += line + "\n" // Add newline for better formatting
-                            }
-
-                            // Check for errors in the error stream
-                            while (errorStreamReader.readLine().also { line = it } != null) {
-                                output += "Rust Error: $line\n" // Prefix Rust errors for clarity
-                            }
-
-                            process.waitFor() // Wait for the process to finish
+                        process.waitFor() // Wait for the process to finish
                         } catch (e: Exception) {
                             val sw = StringWriter()
                             val pw = PrintWriter(sw)
@@ -139,12 +193,12 @@ class DeltaPluginActivity : AppCompatActivity() {
                         return output
                     }
 
-                    private fun copyTextToClipboard() {
-                        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-                        val clipData = ClipData.newPlainText("Shell Output", shellOutputTextView.text.toString())
-                        clipboard.setPrimaryClip(clipData)
+    private fun copyTextToClipboard() {
+    val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    val clipData = ClipData.newPlainText("Shell Output", shellOutputTextView.text.toString())
+        clipboard.setPrimaryClip(clipData)
 
-                        // Optional: Show a toast message to indicate successful copy
-                        Toast.makeText(this, "Shell Output Copied", Toast.LENGTH_SHORT).show()
-                    }
+    // Optional: Show a toast message to indicate successful copy
+    Toast.makeText(this, "Shell Output Copied", Toast.LENGTH_SHORT).show()
+}
 }
