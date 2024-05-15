4bfc0ed20b75c9d60f4650c05d977bdca7ad021c
Jared Davenport <duron27@gmail.com>
2024-05-15 01:38:01 +0000

    blacklist delta mods

--- payload/app/src/main/java/mods/ModsCollection.kt
+++ payload/app/src/main/java/mods/ModsCollection.kt
@@ -138,10 +138,14 @@ class ModsCollection(private val type: ModType,
                blacklist.add("master_index.esp")
                blacklist.add("Siege at Firemoth.esp")
                blacklist.add("delta-merged.omwaddon")
+               blacklist.add("output_deleted.omwaddon")
+               blacklist.add("output_groundcover.omwaddon")
            }
 
            if(type == ModType.Plugin) {
                blacklist.add("delta-merged.omwaddon")
+               blacklist.add("output_deleted.omwaddon")
+               blacklist.add("output_groundcover.omwaddon")
            }
 
      	   // Collect filenames of mods on the FS
