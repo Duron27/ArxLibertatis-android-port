ec086aab549ca0a0f273de46d7174506951f8148
Duron27 <duron27@gmail.com>
2024-06-09 09:14:27 -0400

    fixed icons

--- payload/app/src/main/java/ui/activity/GameActivity.kt
+++ payload/app/src/main/java/ui/activity/GameActivity.kt
@@ -104,6 +104,10 @@ class GameActivity : SDLActivity() {
         if (textureShrinkingOption == "low") Os.setenv("LIBGL_SHRINK", "2", true)
         if (textureShrinkingOption == "medium") Os.setenv("LIBGL_SHRINK", "7", true)
         if (textureShrinkingOption == "high") Os.setenv("LIBGL_SHRINK", "6", true)
+        
+                val avoid16bits = prefs!!.getBoolean("pref_avoid16bits", true)
+        if (avoid16bits == true) Os.setenv("LIBGL_AVOID16BITS", "1", true)
+        else Os.setenv("LIBGL_AVOID16BITS", "0", true)
 
         Os.setenv("OSG_VERTEX_BUFFER_HINT", "VBO", true)
         Os.setenv("OPENMW_USER_FILE_STORAGE", Constants.USER_FILE_STORAGE + "/", true)
